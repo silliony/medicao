849b004d00696d26057e3badd33e29c10148f372
Auanny de Oliveira <130615392+auannyos@users.noreply.github.com>
2025-07-04 12:24:59 -0300

    Update processarIluminancia.m

--- processarIluminancia.m
+++ processarIluminancia.m
@@ -1,6 +1,6 @@
 function iluminancia = processarIluminancia(data)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    tensao = (5 / 1024) * data;
+    tensao = (5 / 1023) * data;
     iluminancia = (tensao * 1000) / 1.54;
 end
